cf05448b54ae1ff24ff979cb96a0f59d331d4733
Eli <elijahwaltonfarrer@gmail.com>
2024-01-03 11:19:41 -0500

    Fixed Input form
    Fixed an issue with the add a movie page where the input boxes were not populating correctly. The issue was my Javascript was dependent on a page load. Because turbo speeds up nav by performing partial page loads when possible, I changed the listener to update on "turbo:load" instead. This solved the problem.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
-import "controllers";
-import "./movies.js";
 import Rails from "@rails/ujs";
 import "jquery";
 import "bootstrap";
+import "controllers";
+import "./movies.js";
 
 
 Rails.start();
--- app/javascript/movies.js
+++ app/javascript/movies.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("turbo:load", function () {
   var numMoviesElement = document.getElementById("num_movies");
 
   if (numMoviesElement) {
     numMoviesElement.addEventListener("change", function (e) {
       var numMovies = e.target.value;
       var container = document.getElementById("additional_movies");
-      container.innerHTML = "";
+      var movieFields = "";
 
       for (var i = 1; i < numMovies; i++) {
-        var movieFields = `
+        movieFields += `
           <div class="movie">
             <label for="movies_${i}_title">Title</label>
             <input type="text" name="movies[${i}][title]" id="movies_${i}_title" placeholder="Title">
@@ -20,9 +20,9 @@ document.addEventListener("DOMContentLoaded", function () {
             <input type="text" name="movies[${i}][image_url]" id="movies_${i}_image_url" placeholder="Image URL">
           </div>
         `;
-
-        container.innerHTML += movieFields;
       }
+
+      container.innerHTML = movieFields;
     });
   }
 });
