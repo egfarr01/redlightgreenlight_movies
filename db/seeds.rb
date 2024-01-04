43b841f1d339605fac81f9e6374435c7dc1b9369
Eli <elijahwaltonfarrer@gmail.com>
2024-01-04 14:17:40 -0500

    Faked some data
    Used the faker gem to fake some data. I created 200 additional users and 2000 reviews. Need to remove integer field from reviews because it's unnecessary.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Generate 100 fake users
+100.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.unique.email,
+    password: Faker::Internet.password(min_length: 10),
+    admin: [true, false].sample
+  )
+end
+
+# Fetch all user IDs who don't have a review yet
+user_ids = User.left_outer_joins(:reviews).where(reviews: { user_id: nil }).pluck(:id)
+movie_ids = Movie.pluck(:id)
+
+# Generate a fake review for each user who doesn't have one yet
+user_ids.each do |user_id|
+  Review.create!(
+    recommendation: [true, false].sample,
+    user_id: user_id,
+    movie_id: movie_ids.sample,
+    integer: Faker::Number.number(digits: 5).to_s
+  )
+end
